d84ecfdfa03670b97aca918a2a220938785505d5
danifc123 <danielcarmo29@gmail.com>
2025-05-02 09:08:04 -0300

    first commit

---
+++ Models/Pagamento.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaAluguel.Models
+{
+    public class Pagamento
+    {
+        
+    }
+}
