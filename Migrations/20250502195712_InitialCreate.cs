4c896a87867d1bf4b54723d1ab9c35943c412404
danifc123 <danielcarmo29@gmail.com>
2025-05-02 16:59:20 -0300

    Updated: migraçao do banco

---
+++ Migrations/20250502195712_InitialCreate.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SistemaAluguel.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contratos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ImovelId = table.Column<int>(type: "integer", nullable: false),
+                    InquilinoId = table.Column<int>(type: "integer", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataFim = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ValorMensal = table.Column<decimal>(type: "numeric", nullable: false),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contratos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Imoveis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Endereco = table.Column<string>(type: "text", nullable: true),
+                    Tipo = table.Column<string>(type: "text", nullable: true),
+                    ValorAluguel = table.Column<decimal>(type: "numeric", nullable: false),
+                    Disponivel = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imoveis", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inquilinos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    CPF = table.Column<string>(type: "text", nullable: true),
+                    Telefone = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inquilinos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ContratoId = table.Column<int>(type: "integer", nullable: false),
+                    MesAnoReferencia = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DataVencimento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Valor = table.Column<decimal>(type: "numeric", nullable: false),
+                    DataPagamento = table.Column<DateOnly>(type: "date", nullable: true),
+                    StatusPagamento = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagamentos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contratos");
+
+            migrationBuilder.DropTable(
+                name: "Imoveis");
+
+            migrationBuilder.DropTable(
+                name: "Inquilinos");
+
+            migrationBuilder.DropTable(
+                name: "Pagamentos");
+        }
+    }
+}
